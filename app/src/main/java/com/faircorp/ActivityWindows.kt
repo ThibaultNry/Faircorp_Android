5f92b1d1579f86bb93420c8f796c3df17ee51324
Thibault Narayana <thibault.narayana.pro@gmail.com>
2021-11-15 19:34:51 +0100

    Initial commit

---
+++ app/src/main/java/com/faircorp/ActivityWindows.kt
@@ -0,0 +1,10 @@
+package com.faircorp
+
+import android.os.Bundle
+
+class ActivityWindows : BasicActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_windows)
+    }
+}
---
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -0,0 +1,30 @@
+package com.faircorp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.TextView
+import com.faircorp.model.WindowAdapter
+
+
+class WindowActivity : BasicActivity() {
+
+    val windowService = WindowService() // (1)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_windows)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.list_windows) // (2)
+        val adapter = WindowAdapter() // (3)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+
+        adapter.update(windowService.findAll()) // (4)
+    }
+}
