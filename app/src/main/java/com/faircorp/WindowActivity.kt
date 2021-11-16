d94327d6a4e99c16c8bc5717aabc037db40c46a8
Thibault Narayana <thibault.narayana.pro@gmail.com>
2021-11-16 19:30:14 +0100

    Avant implémentation list item

--- app/src/main/java/com/faircorp/WindowActivity.kt
+++ app/src/main/java/com/faircorp/WindowActivity.kt
@@ -10,21 +10,8 @@ import com.faircorp.model.WindowAdapter
 
 
 class WindowActivity : BasicActivity() {
-
-    val windowService = WindowService() // (1)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_windows)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.list_windows) // (2)
-        val adapter = WindowAdapter() // (3)
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = adapter
-
-        adapter.update(windowService.findAll()) // (4)
+        setContentView(R.layout.activity_window)
     }
 }
